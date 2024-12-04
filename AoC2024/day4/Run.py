b05c053a6a8ad771a5b3a6291ede5e880bd21207
Siddharth Venkatesan <neotamizhan@gmail.com>
2024-12-04 04:59:53 +0000

    initialized day 4

---
+++ AoC2024/day4/Run.py
@@ -0,0 +1,17 @@
+from AoC2024.utils import load
+
+#problem url: https://adventofcode.com/2024/day/4
+
+def process(daynum):
+  level = 1
+  lines = load(daynum,False)  
+  if level == 1:
+    process_part1(lines)
+  else:
+    process_part2(lines)
+
+def process_part1(lines):
+  return 
+
+def process_part2(lines):
+  return
